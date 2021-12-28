b5df935649e6e747d25715b6132e48ab5fec9f10
Amir Awad <72989304+amir-awad@users.noreply.github.com>
2021-12-29 01:02:13 +0200

    Time: 6 ms (84.89%), Space: 38.8 MB (97.72%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
@@ -9,6 +9,7 @@ class Solution {
         
         ArrayList<Integer> al = new ArrayList<Integer>();
         
+        //it's clear that all the numbers are between 100 and 998 inclusive
         for(int i=100;i<=998;){
             
             boolean flag = exists(i,digits);
@@ -33,10 +34,11 @@ class Solution {
         int d2 = (num/10)%10;
         int d3 = (num/100)%10;
         
-                  
+        //check if num's digits are in arr;          
         boolean f = binarySearch(d1,arr)&&binarySearch(d2,arr)&&binarySearch(d3,arr);
         if(!f) return false;
         
+        //we found out that the digits are in arr ,but what about their occurances? Let's check:
        if(d1!=d2&&d2!=d3&&d1!=d3) return true;
 		if(d1==d2&&d1==d3)
 		{
@@ -65,6 +67,7 @@ class Solution {
         
     }
         
+    // check if some digit is in arr
     public boolean binarySearch(int d,int[] arr){
         int lo=0;
         int hi = arr.length-1;
@@ -82,7 +85,7 @@ class Solution {
         return false;
     }
     
-    
+    // count number of duplicates of an element in arr
     public static int countDups(int d,int[]arr){
         int count=0;
         
