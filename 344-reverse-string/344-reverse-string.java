95021daffb426491d2369f7720e68ea377f6fc95
Amir Awad <72989304+amir-awad@users.noreply.github.com>
2021-12-30 01:53:00 +0200

    Time: 1 ms (94.56%), Space: 45.4 MB (90.29%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int j=s.length-1;
+        for(int i=0;i <j;i++){
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+            j--;
+        }
+    }
+}
