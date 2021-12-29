2739923cf1654c601d3da38d317492d27bb8eea5
Amir Awad <72989304+amir-awad@users.noreply.github.com>
2021-12-29 21:46:38 +0200

    Time: 11 ms (5.92%), Space: 40.1 MB (12.50%) - LeetHub

---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> arr = new ArrayList<List<Integer>>();
+        return levelOrder(root,arr);
+    }
+    
+    public List<List<Integer>> levelOrder(TreeNode root, List<List<Integer>> arr){
+        // if(root==null)
+        //     return;
+        for(int i=0;i <= height(root);i++){
+            List<Integer> al = new ArrayList<Integer>();
+            arr.add(getLevelElems(root,al,0,i));
+        }
+        
+        return arr;
+    }
+    
+    public List<Integer> getLevelElems(TreeNode root,List<Integer> al,int currLev ,int level){
+        if(root==null)
+            return al;
+        if(currLev == level){
+            al.add(root.val);
+        }
+        
+        getLevelElems(root.left,al,currLev+1,level);
+        getLevelElems(root.right,al,currLev+1,level);
+        
+        return al;
+    }
+    
+    public int height(TreeNode root){
+        if(root==null)
+            return -1;
+        int left_Height = height(root.left);
+        int right_Height = height(root.right);
+        
+        return Math.max(left_Height,right_Height) + 1;
+    }
+    
+}
