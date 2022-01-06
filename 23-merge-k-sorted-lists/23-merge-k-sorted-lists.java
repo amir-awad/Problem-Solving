a5d462c80019086fdd1158a062fb910f650b9ef2
Amir Awad <72989304+amir-awad@users.noreply.github.com>
2022-01-06 14:01:55 -0800

    Time: 444 ms (5.01%), Space: 201.4 MB (5.43%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -0,0 +1,86 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0) return null;
+        if(lists.length==1) return lists[0];
+        
+        Stack<ListNode> st = new Stack<ListNode>();
+        for(int i=0;i<lists.length;i++){
+            st.push(lists[i]);
+        }
+        while(true){
+            ListNode curr1 = st.pop();
+            if(st.isEmpty()){
+                st.push(curr1);
+                break;
+            }
+            ListNode curr2 = st.pop();
+            st.push(mergeTwoLists(curr1, curr2));
+        }
+        
+        return st.peek();
+    }
+    
+    
+    
+     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode curr1 = list1;
+        ListNode curr2 = list2;
+        if(list1==null)
+            return list2;
+        if(list2==null)
+            return list1;
+        int min = 0;
+        ListNode resLast=null;
+        ListNode resHead = null;
+        
+        while(curr1!=null&&curr2!=null){
+            
+            if(curr1.val<=curr2.val){
+                min=curr1.val;
+                curr1 = curr1.next;
+                
+            }
+            else{
+                min = curr2.val;
+                curr2 = curr2.next;
+            }
+            
+            if(resLast==null&&resHead==null){
+                resLast=resHead = new ListNode(min);
+               
+            }
+            
+            else {
+                resLast.next = new ListNode(min);
+                resLast = resLast.next;
+            }
+            
+            
+        }
+        
+        while(curr1!=null){
+            resLast.next = new ListNode(curr1.val);
+            curr1 = curr1.next;
+            resLast= resLast.next;
+        }
+        
+        while(curr2!=null){
+            resLast.next = new ListNode(curr2.val);
+            curr2 = curr2.next;
+            resLast= resLast.next;
+        }
+        
+        return resHead;
+        
+    }
+}
