22057a097d4efb58b551eb360c4b42121087aecb
Amir Awad <72989304+amir-awad@users.noreply.github.com>
2022-02-18 05:35:04 +0200

    Time: 9 ms (65.87%), Space: 53.8 MB (14.74%) - LeetHub

---
+++ 455-assign-cookies/455-assign-cookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int j=0;
+        
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int ans=0;
+        
+        for(int i=0;i<g.length&&j<s.length;){
+                
+            if(g[i]<=s[j]){
+                ans++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+                
+        }
+        return ans;
+    }
+}
