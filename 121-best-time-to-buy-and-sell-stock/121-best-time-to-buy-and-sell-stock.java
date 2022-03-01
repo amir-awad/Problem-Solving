76cdcd66ecc022e49a18031fb5d0247d554cd257
Amir Awad <72989304+amir-awad@users.noreply.github.com>
2022-03-01 21:19:20 +0200

    Time: 4 ms (33.00%), Space: 85.7 MB (32.44%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,39 +1,17 @@
 class Solution {
-    static int[] prices;
-    static int[] memo;
     
     public int maxProfit(int[] prices) {
-        this.prices = prices.clone();
-        memo = new int[prices.length];
-        int profit=0;
-        if(prices.length>10000){
-            for(int i=0;i<prices.length;i+=1000){
-                solve(i);
-            }
-        }
-        else{    
-            for(int i=0;i<prices.length;i++){
-                solve(i);
-            }
+        int n = prices.length;
+        int[] max = new int[prices.length];
+        max[n-1] = prices[n-1];
+        for(int i = n-2;i>=0;i--){
+            max[i] = Math.max(max[i+1], prices[i]);
         }
         
-        for(int i=0;i < prices.length;i++)
-            profit = Math.max(profit, memo[i]-prices[i]);
-        
+        int profit =0;
+        for(int j=0;j<prices.length;j++){
+            profit = Math.max(profit, max[j]-prices[j]);
+        }
         return profit;
-        
     }
-    
-    
-    public int solve(int index){
-        if(index == prices.length-1)
-            return prices[prices.length-1];
-        if(index == prices.length)
-            return 0;
-        if(memo[index]!=0)
-            return memo[index];
-        
-        return memo[index] = Math.max(solve(index+1), prices[index]);
-    }
-    
 }
