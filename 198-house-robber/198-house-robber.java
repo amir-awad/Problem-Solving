59ede4b3cbca79e1fec1b194d26d351e3e4c1d80
Amir Awad <72989304+amir-awad@users.noreply.github.com>
2022-03-03 13:31:59 +0200

    Time: 0 ms (100.00%), Space: 39.6 MB (49.63%) - LeetHub

---
+++ 198-house-robber/198-house-robber.java
@@ -0,0 +1,29 @@
+class Solution {
+    static int[] nums;
+    static int[] memo;
+    
+    public int rob(int[] nums) {
+        this.nums = nums;
+        memo = new int[nums.length];  
+        Arrays.fill(memo, -1);
+            
+        return Math.max(solve(0), solve(1));
+        
+    }
+    
+    public static int solve(int i){
+        if(i>=nums.length){
+            return 0;
+        }
+        
+        
+        if(memo[i] !=-1)
+            return memo[i];
+        
+        return memo[i] = nums[i] + Math.max(solve(i+2), solve(i+3));
+        
+    }
+    
+    
+   
+}
