987a56d64eae834750bfb38b56f4c91baf5b95d9
Amir Awad <amirawad004@gmail.com>
2023-10-29 15:32:37 +0200

    Time: 0 ms (100.00%), Space: 40.6 MB (74.98%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode beforeDummyNode = new ListNode();
+        ListNode afterDummyNode = new ListNode();
+        
+        ListNode curr = head;
+        ListNode p1 = beforeDummyNode;
+        ListNode p2 = afterDummyNode;
+        while(curr != null) {
+            if(curr.val < x) {
+                p1.next = curr;
+                p1 = p1.next;
+            } else {
+                p2.next = curr;
+                p2 = p2.next;
+            }
+            
+            curr = curr.next;
+        }
+        
+        p2.next = null;
+        
+        p1.next = afterDummyNode.next;
+        
+        return beforeDummyNode.next;
+    }
+}
