46ad9e7b523240f58683b82e73e67fcfffd8c43c
Amir Awad <amirawad004@gmail.com>
2023-11-03 18:19:33 +0200

    Time: 104 ms (32.33%), Space: 65.3 MB (33.36%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -8,53 +8,24 @@ class NumMatrix {
         this.matrix = matrix;
         this.m = matrix.length;
         this.n = matrix[0].length;
-        this.pref = new int[m][n];
+        this.pref = new int[m + 1][n + 1];
         calculatePref();
     }
     
     public void calculatePref() {
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(i > 0 && j > 0) {
-                    pref[i][j] = pref[i][j - 1] + pref[i - 1][j] - pref[i - 1][j - 1] + matrix[i][j];
-                } else if (i == 0) {
-                    if(j > 0)
-                        pref[i][j] = pref[i][j - 1] + matrix[i][j];
-                    else { 
-                        pref[i][j] = matrix[i][j];
-                    }
-                } else if (j == 0) {
-                    pref[i][j] = pref[i - 1][j] + matrix[i][j];
-                }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                pref[i][j] = pref[i][j - 1] + pref[i - 1][j] - pref[i - 1][j - 1] + matrix[i - 1][j - 1];   
             }
-        }
-    }
-    
-    public void printPref() {
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                System.out.print(pref[i][j] + " ");
-            }
-            System.out.println();
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans = 0;
-        if(row1 > 0) {
-            if(col1 > 0) {
-                ans = pref[row2][col2] - pref[row1 - 1][col2] - pref[row2][col1 - 1] + pref[row1 - 1][col1 - 1];
-            } else {
-                ans = pref[row2][col2] - pref[row1 - 1][col2];
-            }
-        } else {
-            if(col1 > 0) {
-                ans = pref[row2][col2] - pref[row2][col1 - 1];
-            } else {
-                ans = pref[row2][col2];  
-            }
-        }
-        
+        row1++;
+        col1++;
+        row2++;
+        col2++;
+        int ans = pref[row2][col2] - pref[row1 - 1][col2] - pref[row2][col1 - 1] + pref[row1 - 1][col1 - 1];        
         return ans;
     }
 }
