e9415d004e23dc341793a59dfe4bb3464fea57b1
Amir Awad <amirawad004@gmail.com>
2024-08-02 03:09:48 +0300

    Time: 0 ms (100.00%), Space: 45.4 MB (77.70%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,34 +1,51 @@
 class Solution {
-    
-    public static void setZeroesHelper(int[][] matrix, int r, int c, boolean[][] visited) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        //System.out.println(r + " " + c);
-        for(int i = 0; i < m; i++) {
-            if(matrix[i][c] != 0) {
-                matrix[i][c] = 0;
-                visited[i][c] = true;
-            }
-        }
-        //System.out.println(Arrays.deepToString(matrix));
-        for(int j = 0; j < n; j++) {
-            if(matrix[r][j] != 0) {
-                matrix[r][j] = 0;
-                visited[r][j] = true;
-            }
-        }
-    }
-    
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean[][] visited = new boolean[m][n];
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == 0 && !visited[i][j]) {
-                    visited[i][j] = true;
-                    setZeroesHelper(matrix,i, j, visited);
+        
+        boolean firstRowZero = false;
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        
+        boolean firstColumnZero = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColumnZero = true;
+                break;
+            }
+        }
+        
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // row is marked to be set to zeros
+                    matrix[0][j] = 0; // column is marked to be set to zeros
                 }
+            }
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if (firstColumnZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
